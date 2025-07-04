fa1ec196883bcc9992b7ad29b2c9e6b4c62b6abf
Jonah Criswell <jvc.criswell@gmail.com>
2025-07-04 16:11:40 -0400

    Added admin login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
 
     if @user.save
       start_new_session_for @user
-      @user.update!(admin: false)
       redirect_to after_authentication_url, notice: "Account created successfully!"
     else
       render :new
