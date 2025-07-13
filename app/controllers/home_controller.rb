310ed43dc00a4742f58ffaaed6744ddd46018ef3
Jonah Criswell <jvc.criswell@gmail.com>
2025-07-13 18:22:47 -0400

    Furnishly is in a completed state

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,16 @@ class HomeController < ApplicationController
 
   def index
     @featured_products = Product.order('RANDOM()').limit(3)
+    
+    @featured_reviews = {}
+    @featured_products.each do |product|
+    
+      max_rating = product.reviews.maximum(:rating)
+      
+      if max_rating
+        top_reviews = product.reviews.where(rating: max_rating)
+        @featured_reviews[product.id] = top_reviews.sample
+      end
+    end
   end
 end
